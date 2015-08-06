2d729a975b184f367822f2d69c32ccb43d3deeee
Casey Provost <provost.design@gmail.com>
2015-08-05 21:15:17 -0400

    fix user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default to: Efrom: ENV['DEFAULT_FROM_EMAIL']
+  default from: ENV['DEFAULT_FROM_EMAIL']
 
   def invite(user_id)
     @user = User.find(user_id)
