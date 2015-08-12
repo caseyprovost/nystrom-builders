708ec6a2d082542e70b70cd9024cc76045ec875a
Casey Provost <provost.design@gmail.com>
2015-08-12 17:53:10 -0400

    fix credentials for mailgun

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,9 +1,8 @@
 Rails.application.config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
+  address: ENV['SMTP_DOMAIN'],
   port: 587,
-  domain: ENV['SMTP_DOMAIN'],
   authentication: ENV['SMTP_AUTH'],
   enable_starttls_auto: true,
-  user_name: ENV['GMAIL_USERNAME'],
-  password: ENV['GMAIL_PASSWORD']
+  user_name: ENV['EMAIL_USERNAME'],
+  password: ENV['EMAIL_PASSWORD']
 }
